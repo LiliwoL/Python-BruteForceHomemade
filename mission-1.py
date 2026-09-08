21ec18ac004fa967e15b596516355c7650b13408
LiliwoL Fenelon VM <github@liliwol.fr>
2026-09-08 14:15:55 +0200

    update mission

--- mission-1.py
+++ mission-1.py
@@ -5,7 +5,7 @@ from pyfiglet import Figlet
 # Affichage
 figlet = Figlet(font='standard')
 
-print(figlet.renderText("Python BruteForce Homemade"))
+print(figlet.renderText("Python BruteForce Homemade - Mission 1"))
 
 # Infos de la machine cible
 remoteHost="172.16.3.254"
